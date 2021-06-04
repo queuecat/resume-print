61c01daab5d78329dc49a69b923c37c929ea1ae1
queuecat <2356583659@qq.com>
2021-06-04 10:20:54 +0800

    feat:下载模态框

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@ var vm = new Vue({
 	el: '#app',
 	data() {
 		return {
+			// 右侧工具栏中模块显示
 			showModule: [
 				{
 					name: '求职意向',
@@ -60,6 +61,14 @@ var vm = new Vue({
 					children: [],
 				},
 			],
+			// 下载模态框
+			downloadDialogFormVisible: false,
+			downloadForm: {
+				type: 'pdf',
+				downName: '个人简历',
+			},
+			// 照片显示
+			imgShow: true,
 		};
 	},
 	methods: {
@@ -72,9 +81,12 @@ var vm = new Vue({
 			this.showModule.splice(this.hideModule.indexOf(item), 1);
 		},
 		// 下载简历
+		showDownloadDialog() {
+			this.downloadDialogFormVisible = true;
+		},
 		download() {
-			// var shareContent = document.querySelector('#app .mainBox .main'); //需要截图的包裹的（原生的）DOM 对象
-			var shareContent = document.querySelector('.tools'); //需要截图的包裹的（原生的）DOM 对象
+			var shareContent = document.querySelector('#app .mainBox .main'); //需要截图的包裹的（原生的）DOM 对象
+			// var shareContent = document.querySelector('.tools'); //需要截图的包裹的（原生的）DOM 对象
 
 			// 使用html2canvas将dom导出为图片
 			let opts = {
@@ -83,61 +95,46 @@ var vm = new Vue({
 				useCORS: true, // 【重要】开启跨域配置
 			};
 			html2canvas(shareContent, opts).then((canvas) => {
-				// a标签设置donwload属性
-				// let a = document.createElement('a');
-				// a.href = canvas.toDataURL();
-				// a.download = '个人简历';
-				// a.click();
-				// a.remove();
-				// 生成pdf
-				var contentWidth = canvas.width;
-				var contentHeight = canvas.height;
-				//一页pdf显示html页面生成的canvas高度;
-				var pageHeight = (contentWidth / 592.28) * 841.89;
-				//未生成pdf的html页面高度
-				var leftHeight = contentHeight;
-				//页面偏移
-				var position = 0;
-				//a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
-				var imgWidth = 595.28;
-				var imgHeight = (592.28 / contentWidth) * contentHeight;
-				var pageData = canvas.toDataURL('image/jpeg', 1.0);
-				var pdf = new jsPDF('', 'pt', 'a4');
-				//有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
-				//当内容未超过pdf一页显示的范围，无需分页
-				if (leftHeight < pageHeight) {
-					pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight);
-				} else {
-					while (leftHeight > 0) {
-						pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight);
-						leftHeight -= pageHeight;
-						position -= 841.89;
-						//避免添加空白页
-						if (leftHeight > 0) {
-							pdf.addPage();
+				if (this.downloadForm.type === 'pdf') {
+					// 生成pdf
+					var contentWidth = canvas.width;
+					var contentHeight = canvas.height;
+					//一页pdf显示html页面生成的canvas高度;
+					var pageHeight = (contentWidth / 592.28) * 841.89;
+					//未生成pdf的html页面高度
+					var leftHeight = contentHeight;
+					//页面偏移
+					var position = 0;
+					//a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+					var imgWidth = 595.28;
+					var imgHeight = (592.28 / contentWidth) * contentHeight;
+					var pageData = canvas.toDataURL('image/jpeg', 1.0);
+					var pdf = new jsPDF('', 'pt', 'a4');
+					//有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+					//当内容未超过pdf一页显示的范围，无需分页
+					if (leftHeight < pageHeight) {
+						pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight);
+					} else {
+						while (leftHeight > 0) {
+							pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight);
+							leftHeight -= pageHeight;
+							position -= 841.89;
+							//避免添加空白页
+							if (leftHeight > 0) {
+								pdf.addPage();
+							}
 						}
 					}
+					pdf.save(this.downloadForm.downName + '.pdf');
+				} else {
+					// a标签设置donwload属性
+					let a = document.createElement('a');
+					a.href = canvas.toDataURL();
+					a.download = this.downloadForm.downName;
+					a.click();
+					a.remove();
 				}
-				pdf.save('content.pdf');
 			});
-
-			// html转pdf
-			/*	var doc = new jsPDF();
-
-      // We'll make our own renderer to skip this editor
-      var specialElementHandlers = {
-        '#editor': function (element, renderer) {
-          return true;
-        },
-      };
-
-      // All units are in the set measurement for the document
-      // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
-      doc.fromHTML(shareContent, 15, 15, {
-        width: 170,
-        elementHandlers: specialElementHandlers,
-      });
-      doc.save('content.pdf');*/
 		},
 	},
 });
